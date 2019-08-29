4e07e8a76bbaf28db780abb097b150b2ce755ffb
Pavani Pamarthy <pavani.pamarthy52@gmail.com>
2019-08-29 05:57:12 +0000

    Final

--- blur1.rb
+++ blur1.rb
@@ -16,36 +16,7 @@ class Image
     end 
   end 
 
-  def blur(row_index, col_index)
-    @array[row_index -1][col_index] = 1 unless row_index == 0
-    @array[row_index +1][col_index] = 1 unless row_index >= @row_length-1
-    @array[row_index][col_index -1] = 1 unless col_index == 0
-    @array[row_index][col_index +1] = 1 unless col_index >= @col_length-1
-  end
-
-  def find_ones
-    one_pixel = []
-
-    @array.each_with_index do |row_array, row_index|
-        row_array.each_with_index do |cell, col_index|
-        if cell == 1
-          one_pixel << [row_index, col_index]
-        end
-      end
-    end    
-    one_pixel  
-  end
-
-
-  def transform
-    array_coords = find_ones
-
-    array_coords.each do |x, y|
-      blur(x, y)
-    end  
-  end
-
-
+  
 end
 
 
